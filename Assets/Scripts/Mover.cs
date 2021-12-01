b60fcbce5e0b45b48b584241436d348dbdcfa41a
Ismayil Hasanov <ismail@passionpunch.studio>
2021-12-01 12:25:29 +0300

    feat: click-to-move implemented

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,20 +6,23 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-
-    [SerializeField] Transform target;
-
-    Ray lastRay;
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        }
-        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+            MoveToCursor();
+        }        
+    }
 
-        GetComponent<NavMeshAgent>().destination = target.position;
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
     }
 }
