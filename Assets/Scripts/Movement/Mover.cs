01d23aea637c831689fb6ed250c8670c78f761d6
Ismayil Hasanov <ismail@passionpunch.studio>
2021-12-17 11:44:19 +0300

    refactor: introduced IAction interface to finalize the dependency inversion

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 using UnityEngine.AI;
-using RPG.Combat;
 using RPG.Core;
 
 namespace RPG.Movement
 {
-    public class Mover : MonoBehaviour
+    public class Mover : MonoBehaviour, IAction
     {
         NavMeshAgent navMeshAgent;
 
@@ -16,7 +15,6 @@ namespace RPG.Movement
         public void StartMoveAction(Vector3 destination)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            GetComponent<Fighter>().Cancel();
             MoveTo(destination);
         }
 
@@ -27,7 +25,7 @@ namespace RPG.Movement
             navMeshAgent.isStopped = false;
         }
 
-        public void Stop() => navMeshAgent.isStopped = true;
+        public void Cancel() => navMeshAgent.isStopped = true;
 
         private void UpdateAnimator()
         {
