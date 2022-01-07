2920a1dd1f6c831d1cdaac9e877363a5c9cd486c
Ismayil Hasanov <ismail@passionpunch.studio>
2022-01-07 13:56:26 +0300

    minor: look at target enemies when attacking
    also, the player texture changed to blue

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -33,6 +33,7 @@ namespace RPG.Combat
 
         private void AttackBehavior()
         {
+            transform.LookAt(target.transform);
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
                 // This will trigger the Hit() event.
